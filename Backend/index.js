49c2185ca34b8e9a190665a2b4dc1e54f27db56e
Rudra Pratap Singh <rudrapratap7488@gmail.com>
2026-07-13 16:28:52 +0530

    cors error

--- Backend/index.js
+++ Backend/index.js
@@ -24,7 +24,7 @@ connectDB();
 const __dirname = path.resolve();
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175'], // Allow multiple ports for dev
+  origin: true,
   credentials: true,
 }));
 app.use(express.json());
